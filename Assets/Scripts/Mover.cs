fa06d0af0c7085fe01e18799255ec68ee40a435f
Marcin Biegun <marcin@mm.st>
2018-09-22 10:02:05 +0200

    Add scripts for controlling player and rockets

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public Vector3 move = new Vector3(0f, 0f, 0f);
+
+	void Update () {
+        gameObject.transform.position += move * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Direction {
+    Left,
+    Right,
+};
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed = 0.1f;
+    public bool fireLocked = false;
+
+    public GameObject rocketPrefab;
+    public GameObject rocketsHolder;
+
+    // Use this for initialization
+    void Start() {
+        rocketsHolder = new GameObject(name = "RocketsHolder");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            gameObject.transform.position += MoveVector(Direction.Left);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            gameObject.transform.position += MoveVector(Direction.Right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Fire();
+        }
+    }
+
+    Vector3 MoveVector(Direction direction) {
+        float delta = Time.deltaTime * moveSpeed;
+        switch (direction) {
+            case Direction.Left:
+                return new Vector3(-delta, 0f, 0f);
+            case Direction.Right:
+                return new Vector3(delta, 0f, 0f);
+        }
+        throw new UnityException("Unknown direction");
+    }
+
+    void Fire() {
+        if (fireLocked)
+            return;
+        GameObject newProjectile = Instantiate(rocketPrefab, gameObject.transform);
+        newProjectile.transform.SetParent(rocketsHolder.transform);
+        // fireLocked = reue
+    }
+
+}
