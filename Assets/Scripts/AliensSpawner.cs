ab831f89d4e385c20d52c885c0d8b839da39ad57
Marcin Biegun <marcin@mm.st>
2018-09-23 19:15:10 +0200

    Add aliens lasers

--- Assets/Scripts/AliensSpawner.cs
+++ Assets/Scripts/AliensSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class AliensSpawner : MonoBehaviour {
     public GameObject alienPrefab;
@@ -11,7 +12,19 @@ public class AliensSpawner : MonoBehaviour {
     public float alienColumns = 10f;
     public float alienRows = 3f;
 
+    public GameObject laserPrefab;
+    public float shotDelaySeconds = 1f;
+
+    public GameObject aliensHolder;
+    private float lastShootAt = 0f;
+    private GameObject lasersHolder;
+
     void Start () {
+        lasersHolder = new GameObject("LasersHolder");
+        aliensHolder = new GameObject(name: "AliensHolder");
+        // Make a child, so aliens will move along this game object
+        aliensHolder.transform.SetParent(gameObject.transform);
+
         float minX = gameObject.transform.position.x - width / 2f;
         float stepX = width / (alienColumns-1);
         float minY = gameObject.transform.position.y - height / 2f;
@@ -23,11 +36,25 @@ public class AliensSpawner : MonoBehaviour {
                 float offsetY = y * stepY;
                 Vector3 position = new Vector3(minX + offsetX, minY + offsetY, 0f);
                 GameObject newAlien = Instantiate(alienPrefab, position, Quaternion.identity);
-                newAlien.transform.SetParent(gameObject.transform);
+                newAlien.transform.SetParent(aliensHolder.transform);
             }
         }
     }
 
     void Update () {
+        Shot();
 	}
+
+    void Shot() {
+        if (lastShootAt > (Time.time - shotDelaySeconds))
+            return;
+
+        var randomIndex = Random.Range(0, aliensHolder.transform.childCount);
+        var randomAlien = aliensHolder.transform.GetChild(randomIndex);
+
+        GameObject newLaser = Instantiate(laserPrefab, randomAlien.transform);
+        newLaser.transform.SetParent(lasersHolder.transform);
+
+        lastShootAt = Time.time;
+    }
 }
---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col) {
+        int boundsLayer = LayerMask.NameToLayer("Bounds");
+        int alienLayer = LayerMask.NameToLayer("Alien");
+        int cityLayer = LayerMask.NameToLayer("City");
+
+        if (col.gameObject.layer == boundsLayer) {
+            Destroy(gameObject);
+        } else if (col.gameObject.layer == cityLayer) {
+            Destroy(gameObject);
+            col.gameObject.GetComponent<CityPixelController>().DetachPixelFromCity();
+        }
+    }
+}
